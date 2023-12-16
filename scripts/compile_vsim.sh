1a33cc8174505b288b189129397e7cb3fbe10731
Chaoqun Liang <chaoliang@lagrev4.ee.ethz.ch>
2023-12-16 20:52:02 +0100

    add rtl target

--- scripts/compile_vsim.sh
+++ scripts/compile_vsim.sh
@@ -18,7 +18,7 @@ set -e
 
 [ ! -z "$VSIM" ] || VSIM=vsim
 
-bender script vsim -t eth_test -D ETHERNET_TESTBENCH \
+bender script vsim -t rtl -t eth_test -D ETHERNET_TESTBENCH \
     --vlog-arg="-svinputport=compat" \
     --vlog-arg="-override_timescale 1ns/1ps" \
     --vlog-arg="-suppress 2583" \
